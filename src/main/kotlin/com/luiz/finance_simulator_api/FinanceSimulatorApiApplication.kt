03aef3fca5a3de2e2e874ff1c8edb5e64da1b45c
Luiz Nicolau Lemos Freitas <luiznicolaulemosfreitas@gmail.com>
2025-05-02 20:28:25 -0300

    first commit

---
+++ src/main/kotlin/com/luiz/finance_simulator_api/FinanceSimulatorApiApplication.kt
@@ -0,0 +1,11 @@
+package com.luiz.finance_simulator_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceSimulatorApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceSimulatorApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/luiz/finance_simulator_api/FinanceSimulatorApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.luiz.finance_simulator_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceSimulatorApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
