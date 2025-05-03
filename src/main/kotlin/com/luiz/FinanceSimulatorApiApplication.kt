2e95c14dbf5f029fe9cd073d6802a5b633c5451b
Luiz Nicolau Lemos Freitas <luiznicolaulemosfreitas@gmail.com>
2025-05-02 21:33:38 -0300

    :construction_worker: testing linter formatting

--- src/main/kotlin/com/luiz/FinanceSimulatorApiApplication.kt
+++ src/main/kotlin/com/luiz/FinanceSimulatorApiApplication.kt
@@ -6,4 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class FinanceSimulatorApiApplication
 
-fun main(args: Array<String>) { runApplication<FinanceSimulatorApiApplication>(*args)}
+fun main(args: Array<String>) {
+    runApplication<FinanceSimulatorApiApplication>(*args)
+}
